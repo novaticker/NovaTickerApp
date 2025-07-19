3d6e5e5213182a8102761a16310c68f7d1fa95a5
novaticker <lds811311@gmail.com>
2025-07-19 12:21:02 +0900

    Update MainActivity.java

--- app/src/main/java/com/novaticker/MainActivity.java
+++ app/src/main/java/com/novaticker/MainActivity.java
@@ -1,1 +1,29 @@
+package com.novaticker;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView newsTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        newsTextView = findViewById(R.id.newsTextView);
+        Button refreshButton = findViewById(R.id.refreshButton);
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newsTextView.setText("최신 뉴스를 불러오는 중입니다...\n\n- 뉴스1: FDA 승인 소식\n- 뉴스2: 인수합병 발표\n- 뉴스3: 대형 투자 유치");
+            }
+        });
+    }
+}
