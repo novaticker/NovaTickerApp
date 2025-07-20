5a3c58287e584eb18325ccb9bf3d21399fff0636
novaticker <lds811311@gmail.com>
2025-07-20 10:58:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template, send_from_directory, jsonify
 import os
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/positive_news.json')
-def positive_news():
-    return app.send_static_file("positive_news.json")
-
-@app.route('/rising_stocks.json')
+@app.route("/rising_stocks.json")
 def rising_stocks():
-    return app.send_static_file("rising_stocks.json")
+    path = "rising_stocks.json"
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as f:
+            return jsonify(json.load(f))
+    else:
+        return jsonify({"results": [], "updated": "정보 없음"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/positive_news.json")
+def positive_news():
+    path = "positive_news.json"
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as f:
+            return jsonify(json.load(f))
+    else:
+        return jsonify({})
+
+@app.route("/static/<path:filename>")
+def static_files(filename):
+    return send_from_directory("static", filename)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
